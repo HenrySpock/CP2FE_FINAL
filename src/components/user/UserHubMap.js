1715fe9ac01086b3bad9866fe5e78a1076b0db3c
HenrySpock <motiviquestudio@gmail.com>
2024-01-25 04:24:11 -0600

    final css

--- src/components/user/UserHubMap.js
+++ src/components/user/UserHubMap.js
@@ -175,7 +175,7 @@ function UserHubMap({ travelogs, trips }) {
             onChange={(e) => setUserZoom(parseInt(e.target.value, 10))}
           />
           <button onClick={() => handleZoomChange(userZoom)} className='zoomlevel-btn'>Update</button>
-          <button onClick={() => handleZoomChange(4)} className='userhubmap-btn'>Default</button>
+          <button onClick={() => handleZoomChange(4)} className='zoomlevel-btn'>Default</button>
         </div>
       )} 
     </div>
